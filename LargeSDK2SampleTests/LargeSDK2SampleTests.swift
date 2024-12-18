7d803832a03844db324b4164ea56ea184bb11f21
Amol Deshmukh <amol.deshmukh@idmission.com>
2024-12-18 11:49:38 +0530

    Initial Commit

---
+++ LargeSDK2SampleTests/LargeSDK2SampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  LargeSDK2SampleTests.swift
+//  LargeSDK2SampleTests
+//
+//  Created by Amol Deshmukh on 18/12/24.
+//
+
+import Testing
+@testable import LargeSDK2Sample
+
+struct LargeSDK2SampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
